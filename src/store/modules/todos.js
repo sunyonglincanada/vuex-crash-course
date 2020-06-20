c3f3f28de466f2c1ce2ab6d1ca6afa50bf2fc50e
Eric Sun <sunyonglincanada@gmail.com>
2020-06-19 20:58:58 -0400

    addTodo action and mutation.

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -24,10 +24,18 @@ const actions = {
     );
     commit("setTodos", response.data);
   },
+  async addTodo({ commit }, title) {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/todos/",
+      { title, completed: false }
+    );
+    commit("newTodo", response.data);
+  },
 };
 
 const mutations = {
   setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo),
 };
 
 export default {
